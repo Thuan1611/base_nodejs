6f8a0ecde7423eb0d5a7cd949ca824e0dac19eab
Thuan1611 <huythuan0977686@gmail.com>
2025-05-15 16:19:47 +0700

    setup base

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import { PORT } from "./src/configs/enviroments.js";
 import jsonValid from "./src/middlewares/jsonInvalid.js";
 import setupSwagger from "./src/configs/swaggerConfig.js";
+import { formatResponseSuccess } from "./src/middlewares/formatResponseSuccess.js";
 
 const app = express();
 app.use(express.json());
@@ -14,33 +15,36 @@ app.use(express.json());
 connectDB();
 
 app.use(
-	cors({
-		origin: ["http://localhost:5173", "http://localhost:5174"],
-		credentials: true,
-		// Them cac cau hinh can thiet
-	})
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+    // Them cac cau hinh can thiet
+  })
 );
 
 setupSwagger(app);
 
-app.use("/api", routes);
+// Middleware xử lý format dữ liệu JSON trả về
+app.use(formatResponseSuccess);
 
 // Middleware xử lý JSON không hợp lệ
 app.use(jsonValid);
 
-// Middleware xử lý route không tồn tại
-app.use(notFoundHandler);
-
 // Middleware xử lý lỗi chung
 app.use(errorHandler);
 
+app.use("/api", routes);
+
+// Middleware xử lý route không tồn tại
+app.use(notFoundHandler);
+
 const server = app.listen(PORT, () => {
-	console.log(`Server is running on: http://localhost:${PORT}/api`);
-	console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
+  console.log(`Server is running on: http://localhost:${PORT}/api`);
+  console.log(`Swagger Docs available at http://localhost:${PORT}/api-docs`);
 });
 
 // Middleware xử lý lỗi không xác định
 process.on("unhandledRejection", (error, promise) => {
-	console.error(`Error: ${error.message}`);
-	server.close(() => process.exit(1));
+  console.error(`Error: ${error.message}`);
+  server.close(() => process.exit(1));
 });
