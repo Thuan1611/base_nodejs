461e66915038ee0e7762b109943d2f3f71f5997f
Thuan1611 <huythuan0977686@gmail.com>
2025-06-03 08:37:21 +0700

    WIP: Đang chỉnh sửa index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import productsRouter from "./productsRouter.js";
 import categoryRoutes from "./categoryRoutes.js";
+import variantRoutes from "./variantRoutes.js";
+import userRoutes from "./users.routes.js";
+import emailRouters from "./emailRoutes.js";
 
 const routes = Router();
 
@@ -11,5 +14,10 @@ routes.use("/products", productsRouter)
 // routes.use("/products", hanldeProduct...)
 
 routes.use("/categories", categoryRoutes);
+routes.use("/variants", variantRoutes);
+
+routes.use("/users", userRoutes);
+routes.use("/emails", emailRouters);
+
 
 export default routes;
